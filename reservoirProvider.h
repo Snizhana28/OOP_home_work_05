afe6ed3d1f364d3627728f7deb70d56bbd6b68ae
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-03-24 20:52:00 +0200

    provider

--- reservoirProvider.h
+++ reservoirProvider.h
@@ -1,1 +1,17 @@
-#pragma once
+#include "reservoir.h"
+
+class ReservoirProvider
+{
+public:
+	static void showReservoir(Reservoir* obj, int size)
+	{
+		for (size_t i = 0; i < size; i++)
+		{
+			cout << "Name : " << obj[i].get_name() << endl;
+			cout << "Type : " << obj[i].get_type() << endl;
+			cout << "Width (km): " << obj[i].get_width() << endl;
+			cout << "Lenght (km): " << obj[i].get_lenght() << endl;
+			cout << "Max depth (m): " << obj[i].get_max_depth() << endl << endl;
+		}
+	}
+};
